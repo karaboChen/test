701c0cb8b7dadafdb0af49001b7ea813a1c45466
karaboChen <nn0907137897@gmail.com>
2023-06-13 19:50:46 +0800

    Updates

--- assets/backend-c2d93911.js
+++
@@ -1,1 +0,0 @@
-import{B as o}from"./index-7e59a937.js";const s=o.create({baseURL:"https://vue3-course-api.hexschool.io/",timeout:5e3});export{s as i};
--- assets/component-functions-4dab0694.js
+++ assets/component-functions-4dab0694.js
@@ -1,4 +1,4 @@
-import{q as T}from"./index-7e59a937.js";var $={},et={get exports(){return $},set exports(p){$=p}};/*!
+import{q as T}from"./index-cc47edcd.js";var $={},et={get exports(){return $},set exports(p){$=p}};/*!
   * Bootstrap index.js v5.2.3 (https://getbootstrap.com/)
   * Copyright 2011-2022 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
